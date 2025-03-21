38fbf086375e786f3168455feef9bf6571ef49b0
Akshay Garg <akshaygarg@Akshays-MacBook-Air.local>
2025-03-21 19:37:53 +0530

    Added winner checking logic

--- src/components/ticTacToe/TicTacToe.jsx
+++ src/components/ticTacToe/TicTacToe.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import "./TicTacToe.css";
 
+const PLAYER_X = "X";
+const PLAYER_O = "O";
+const DRAW = "Draw";
+
 const TicTacToe = ({ size }) => {
   // board will have possible values of null, X, O
   const [board, setBoard] = useState(
@@ -10,42 +14,78 @@ const TicTacToe = ({ size }) => {
   );
   // makes an assumption that X always starts the game
   const [isXNext, setIsXNext] = useState(true);
-  // possible states - null, X, O
+  // possible states - null, X, O, Draw
   const [winner, setWinner] = useState(null);
+
+  const calculateWinner = (rowIndex, columnIndex, player, newBoard) => {
+    let winRow = true;
+    let winCol = true;
+    let winDiag = true;
+    let winRevDiag = true;
+
+    for (let i = 0; i < size; i++) {
+      if (newBoard[rowIndex][i] !== player) {
+        winRow = false;
+      }
+      if (newBoard[i][columnIndex] !== player) {
+        winCol = false;
+      }
+      if (newBoard[i][i] !== player) {
+        winDiag = false;
+      }
+      if (newBoard[i][size - 1 - i] !== player) {
+        winRevDiag = false;
+      }
+    }
+
+    if (winRow || winCol || winDiag || winRevDiag) {
+      return player;
+    }
+
+    if (newBoard.flat().every((cell) => cell)) {
+      return DRAW;
+    }
+  };
 
   const handleClick = (rowIndex, columnIndex) => {
     // ignore if already filled
-    if (board[rowIndex][columnIndex]) {
+    if (board[rowIndex][columnIndex] || winner) {
       return;
     }
 
     // deep copy for immutability
     const newBoard = board.map((row) => [...row]);
-    const player = isXNext ? "X" : "O";
+    const player = isXNext ? PLAYER_X : PLAYER_O;
     newBoard[rowIndex][columnIndex] = player;
     setBoard(newBoard);
 
-    // calculateWinner
+    const gamer = calculateWinner(rowIndex, columnIndex, player, newBoard);
 
-    // only if winner is not found yet. Otherwise stop!
-    setIsXNext((prev) => !prev);
+    if (gamer) {
+      setWinner(gamer);
+    } else {
+      setIsXNext((prev) => !prev);
+    }
   };
 
   return (
-    <div className="board">
-      {board.map((row, rowIndex) => {
-        return row.map((cell, columnIndex) => {
-          return (
-            <button
-              className="cell"
-              key={`${rowIndex}:${columnIndex}`}
-              onClick={() => handleClick(rowIndex, columnIndex)}
-            >
-              {cell}
-            </button>
-          );
-        });
-      })}
+    <div>
+      <h1>Winner: {winner}</h1>
+      <div className="board">
+        {board.map((row, rowIndex) => {
+          return row.map((cell, columnIndex) => {
+            return (
+              <button
+                className="cell"
+                key={`${rowIndex}:${columnIndex}`}
+                onClick={() => handleClick(rowIndex, columnIndex)}
+              >
+                {cell}
+              </button>
+            );
+          });
+        })}
+      </div>
     </div>
   );
 };
